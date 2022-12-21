e287e0252fee5d6ff428644a9752302a08594e7c
Siarhei <siarhei.by@gmail.com>
2022-12-22 00:36:47 +0300

    feat: add background for side menu

--- js/index.js
+++ js/index.js
@@ -1,17 +1,21 @@
 // Adaptive menu
-const hamburger = document.querySelector('.hamburger');
-const nav = document.querySelector('.nav__list');
-const navLinks = document.querySelectorAll('.nav__link');
+const HAMBURGER = document.querySelector('.hamburger');
+const NAV = document.querySelector('.nav__list');
+const BACKGOUND = document.querySelector('.background');
+const NAV_LINKS = document.querySelectorAll('.nav__link');
 
 function toggleMenu () {
-    hamburger.classList.toggle('open');
-    nav.classList.toggle('open');
+    HAMBURGER.classList.toggle('open');
+    NAV.classList.toggle('open');
+    BACKGOUND.classList.toggle('visible');
 }
 
 function closeMenu() {
-    hamburger.classList.remove('open');
-    nav.classList.remove('open');
+    HAMBURGER.classList.remove('open');
+    NAV.classList.remove('open');
+    BACKGOUND.classList.remove('visible');
 }
 
-hamburger.addEventListener('click', toggleMenu);
-navLinks.forEach((link) => link.addEventListener('click', closeMenu));
+HAMBURGER.addEventListener('click', toggleMenu);
+BACKGOUND.addEventListener('click', closeMenu);
+NAV_LINKS.forEach((link) => link.addEventListener('click', closeMenu));
